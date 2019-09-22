bff0e4b8b98365e1a166818c9963dec2deb30b60
Prateek <joshi2401@gmail.com>
2019-09-22 23:35:25 +0530

    Committing the initial sample project

---
+++ Algorithms/src/main/java/com/datastructures/Algorithms/App.java
@@ -0,0 +1,13 @@
+package com.datastructures.Algorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
